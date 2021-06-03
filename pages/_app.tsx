73d4d9ecb46b0524b3bd650f69f4a1caf49e2743
m19e <nexus.me7290@gmail.com>
2021-06-03 18:58:26 +0900

    Typing app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
 export default MyApp;
