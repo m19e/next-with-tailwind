8d22e16d1725956eca25fea6b45d43bffd437382
m19e <nexus.me7290@gmail.com>
2021-06-03 19:02:38 +0900

    Provide theme to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+    <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+    </ThemeProvider>
+);
 
 export default MyApp;
